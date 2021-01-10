1f9e65d4da41d9ac9d3a0d08503f9756b91f1381
Lukáš Chylík <lukas.chylik@riganti.cz>
2021-01-10 17:37:53 +0100

    project created

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+﻿const webpack = require('webpack');
+const path = require('path');
+const ScriptsPath = "./Resources/Scripts";
+
+module.exports = {
+    entry: {
+        init: [`${ScriptsPath}/init.ts`]
+    },
+    output: {
+        chunkLoading: false,
+        wasmLoading: false,
+        filename: '[name].js',
+        path: path.resolve(__dirname, './wwwroot/js')
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader',
+                }]
+            }
+        ]
+    },
+    devtool: 'source-map'
+};
---
+++ wwwroot/js/init.js
@@ -0,0 +1,11 @@
+/******/ (function() { // webpackBootstrap
+/*!***********************************!*\
+  !*** ./Resources/Scripts/init.ts ***!
+  \***********************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+console.log('Hello World!');
+
+/******/ })()
+;
+//# sourceMappingURL=init.js.map
